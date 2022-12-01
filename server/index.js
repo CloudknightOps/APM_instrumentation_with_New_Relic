8459bd78e7d8fe47b7d48a1f7297030ba69bb3a7
Glitch (nru-foodme) <none>
2022-12-01 23:02:43 +0000

    🐑🙍 Checkpoint ./package.json:2697146/21 ./server/index.js:2697146/57

--- server/index.js
+++ server/index.js
@@ -60,7 +60,7 @@ exports.start = function(PORT, STATIC_DIR, DATA_FILE, TEST_DIR) {
   });
 
   app.post(API_URL_ORDER, jsonParser, function(req, res, next) {  
-    logger.info(req.body);
+    logger.info('body %o', req.body);
     /*
     var order = req.body;
     var itemCount = 0;
